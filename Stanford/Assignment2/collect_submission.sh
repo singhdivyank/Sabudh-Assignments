854551d1491fbbc68dcb14a7f99d5dbe8b7702ea
Divyank Singh <41583221+singhdivyank@users.noreply.github.com>
2020-08-14 18:25:11 +0530

    Add files via upload

---
+++ Stanford/Assignment2/collect_submission.sh
@@ -0,0 +1,4 @@
+rm -f assignment2.zip
+# zip -r assignment2.zip .*.py .*.png saved_params_40000.npy
+7z a -bt a.7z *.py *.png saved_params_40000.npy
+# assignment2.zip .*.py .*.png saved_params_40000.npy
