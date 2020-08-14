1ce99141973fb62aa396d21846cbd90c895bcffb
Divyank Singh <41583221+singhdivyank@users.noreply.github.com>
2020-08-14 18:18:52 +0530

    Delete collect_submission.sh

--- Stanford/collect_submission.sh
+++
@@ -1,4 +0,0 @@
-rm -f assignment2.zip
-# zip -r assignment2.zip .*.py .*.png saved_params_40000.npy
-7z a -bt a.7z *.py *.png saved_params_40000.npy
-# assignment2.zip .*.py .*.png saved_params_40000.npy
