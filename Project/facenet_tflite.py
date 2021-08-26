4f607dcc69fddd50adfc9a32a7dfe633ddc874ce
Divyank Singh <41583221+singhdivyank@users.noreply.github.com>
2021-08-26 16:30:41 +0530

    Delete Project directory

--- Project/facenet_tflite.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from tensorflow.contrib import lite
-
-converter = lite.TFLiteConverter.from_keras_model_file('facenet_keras.h5')
-model = converter.convert()
-file = open('model.tflite', 'wb')
-file.write(model)
